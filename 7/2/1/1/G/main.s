e01ff6993ffc336272adf2cac1f47a4d71b3cc46
Ben Lirio <benjlirio@gmail.com>
2021-11-14 18:54:53 +0000

    Add another way of viewing the gray binary code

--- 7/2/1/1/G/main.s
+++ 7/2/1/1/G/main.s
@@ -1,3 +1,39 @@
+/*
+Gray binary generation
+*/
+print_binary:
+    str lr, [sp, -16]!
+    sub sp, sp, 80
+    mov w10, 10
+    strb w10, [sp, 64]
+    mov x10, 0
+1:  cmp x10, 8
+    bge 1f
+    mov x14, 0
+    mov x11, 0
+2:  cmp x11, 8
+    bge 2f
+    mov x12, 43
+    tst x1, 1
+    csel x12, x12, xzr, ne
+    lsr x1, x1, 1
+    lsl x14, x14, 8
+    orr x14, x14, x12
+    add x11, x11, 1
+    b   2b
+2:
+    rev x14, x14
+    str x14, [sp, x10, lsl 3]
+    add x10, x10, 1
+    b   1b
+1:
+    mov x0, 1
+    mov x1, sp
+    mov x2, 65
+    bl  write
+    add sp, sp, 80
+    ldr lr, [sp], 16
+    ret
     .global main
 main:
     str lr, [sp, -16]!
@@ -6,7 +42,7 @@ main:
 G1: mov a, 0
 G2: adr x0, fmt
     mov x1, a, lsr 1
-    bl printf
+    bl print_binary
 G3: tst a, 1
     cinc a, a, eq
     cinv a, a, ne
@@ -16,7 +52,7 @@ G4: rbit a, a
     clz j, a
     add j, j, 1
     rbit a, a
-G5: cmp j, 4
+G5: cmp j, 64
     bge Done
     mov x10, 1
     lsl x10, x10, j
@@ -34,4 +70,6 @@ Done:
     ret
 .data
 fmt: .asciz "%d\n"
+msg: .asciz "hello world\n"
+.equ msg_len, (.-msg)
 .bss
